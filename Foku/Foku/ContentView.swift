d1bb32e22ffdca1bc451dc7aeacf45ad8df4791f
Augustas Stančikas <augustas.stancikas@gmail.com>
2026-07-07 18:37:32 +0300

    Create initial Xcode macOS app

---
+++ Foku/Foku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foku
+//
+//  Created by Augustas Stančikas on 07/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foku/Foku/FokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FokuApp.swift
+//  Foku
+//
+//  Created by Augustas Stančikas on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct FokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
